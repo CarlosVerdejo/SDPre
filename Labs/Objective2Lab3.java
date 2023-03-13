b2dda3a089b8e1f106f9b292614e2e0f128a74b8
CarlosVerdejo <carlosverdejo77@yahoo.com>
2023-03-13 09:53:09 -0400

    complete Objective2Lab

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int birth_year = 1992;
+    String month = "December ";
+    boolean weekday = (2 > 1); 
+
+    System.out.println(birth_year);
+    System.out.println(month);
+    System.out.println(weekday);
+  }
+}
